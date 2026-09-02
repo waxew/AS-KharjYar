fb4fa97b685f3616e9884a923bdb508d640577b5
waxew <4622.ar@gmail.com>
2026-09-02 13:27:52 +0330

    feat: add Persian locale code

--- app/src/main/java/com/wisnu/kurniawan/wallee/foundation/localization/LanguageCode.kt
+++ app/src/main/java/com/wisnu/kurniawan/wallee/foundation/localization/LanguageCode.kt
@@ -1,6 +1,7 @@
 package com.wisnu.kurniawan.wallee.foundation.localization
 
 object LanguageCode {
+    const val PERSIAN: String = "fa"
     const val ENGLISH: String = "en"
     const val INDONESIA: String = "id"
 }

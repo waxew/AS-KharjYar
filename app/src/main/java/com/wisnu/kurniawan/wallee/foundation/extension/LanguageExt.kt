cbbd7c5730bc71eec5e22f5426b7ee41a9be540a
waxew <4622.ar@gmail.com>
2026-09-02 13:29:02 +0330

    feat: make Persian the default localized language

--- app/src/main/java/com/wisnu/kurniawan/wallee/foundation/extension/LanguageExt.kt
+++ app/src/main/java/com/wisnu/kurniawan/wallee/foundation/extension/LanguageExt.kt
@@ -3,9 +3,15 @@ package com.wisnu.kurniawan.wallee.foundation.extension
 import com.wisnu.kurniawan.wallee.foundation.datasource.preference.model.LanguagePreference
 import com.wisnu.kurniawan.wallee.model.Language
 
+/**
+ * Converts the persisted language code to the app language model.
+ * Persian is the safe default for empty/new preferences in the AS Team build.
+ */
 fun LanguagePreference.toLanguage(): Language {
     return when (this.code) {
+        Language.PERSIAN.code -> Language.PERSIAN
+        Language.ENGLISH.code -> Language.ENGLISH
         Language.INDONESIA.code -> Language.INDONESIA
-        else -> Language.ENGLISH
+        else -> Language.PERSIAN
     }
 }

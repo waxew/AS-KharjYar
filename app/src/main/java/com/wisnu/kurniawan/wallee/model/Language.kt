43b8869dedf2df217f1aef3f74b582b155fb27a3
waxew <4622.ar@gmail.com>
2026-09-02 13:28:04 +0330

    feat: register Persian as an app language

--- app/src/main/java/com/wisnu/kurniawan/wallee/model/Language.kt
+++ app/src/main/java/com/wisnu/kurniawan/wallee/model/Language.kt
@@ -3,6 +3,7 @@ package com.wisnu.kurniawan.wallee.model
 import com.wisnu.kurniawan.wallee.foundation.localization.LanguageCode
 
 enum class Language(val code: String) {
+    PERSIAN(LanguageCode.PERSIAN),
     ENGLISH(LanguageCode.ENGLISH),
     INDONESIA(LanguageCode.INDONESIA),
 }

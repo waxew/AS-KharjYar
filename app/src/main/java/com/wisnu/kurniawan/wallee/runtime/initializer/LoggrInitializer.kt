8ffd8d8f6507d327de16168ded52a1ce88a4c417
waxew <4622.ar@gmail.com>
2026-09-02 12:31:36 +0330

    security: disable upstream crash reporting

--- app/src/main/java/com/wisnu/kurniawan/wallee/runtime/initializer/LoggrInitializer.kt
+++ app/src/main/java/com/wisnu/kurniawan/wallee/runtime/initializer/LoggrInitializer.kt
@@ -3,13 +3,18 @@ package com.wisnu.kurniawan.wallee.runtime.initializer
 import android.content.Context
 import androidx.startup.Initializer
 import com.wisnu.foundation.coreloggr.Loggr
-import com.wisnu.foundation.libanalyticsmanager.crash.CrashLogging
 import com.wisnu.foundation.testdebug.DebugTools
 
+/**
+ * Initializes local/debug logging for the AS Team build.
+ *
+ * Upstream Firebase Crashlytics logging is intentionally not registered here. Production
+ * crash reporting can be connected later to an AS Team-owned service without leaking data
+ * to the upstream Firebase project.
+ */
 class LoggrInitializer : Initializer<Loggr> {
     override fun create(context: Context): Loggr {
         val loggings = DebugTools.getLoggings().toMutableList()
-        loggings.add(CrashLogging())
         Loggr.initialize(loggings)
         return Loggr
     }
